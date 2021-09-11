4a95a9e64201389c6845c3bc301dc7bb28e458da
Lucas <abad.lucas.b@gmail.com>
2021-09-12 01:35:47 +0200

    Programa creado para la prueba de Git Cambio realizado eliminando el salto de linea y la comporbacion de la suma

---
+++ Pruebas/test.py
@@ -0,0 +1,16 @@
+# "Primer programa de prueba en Python y con Atom"
+
+# Se trata de un programa que pide dos numeros para una Suma y escribe
+# tantos nombres como el resultado de la Suma
+
+num1 = int(input("Introduce el primer sumando: "))
+num2 = int(input("Introduce el segundo sumando: "))
+sum = num1 + num2
+
+print("La suma es : ",num1," + ",num2," = ",sum)
+
+nombre = input("Introduzca tu nombre: ")
+
+while (sum > 0):
+    print(nombre)
+    sum -= 1
