5627f273bcb4ef74e46a2c37b61deda5389eb9c4
Lucas <abad.lucas.b@gmail.com>
2021-09-12 16:54:55 +0200

    Cambio de oraganizacion

---
+++ Curso/Entrada-Salida/AreaTriangulo.py
@@ -0,0 +1,9 @@
+'''
+    AreaTriangulo.py
+    Script que realiza el Area de un Triangulo si se le da la base y la altura.
+'''
+
+base = int(input("Introduzca la base del triangulo: \t"))
+altura = int(input("Introduzca la altura del triangulo: \t"))
+
+print("El area del triangulo dado es de : ", (base*altura)/2)
