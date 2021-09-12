6b8e25a2594acaacd9ff6479e452fe451b6f460d
Lucas <abad.lucas.b@gmail.com>
2021-09-12 16:42:09 +0200

    Juego terminado, de manera secuencial

--- Curso/Entrada-Salida.py
+++ Curso/Entrada-Salida.py
@@ -3,3 +3,18 @@
     Script en Python que implementa un pequeño juego para manejar la Entrada
     y Salida de la consola por el Usuario.
 '''
+
+# mensaje a pantalla con pritn()
+print("Comencemos el juego \n\
+Te voy a solicitar que realices unas operaciones y \
+posteriormente adivinare el numero que tienes en mente. ")
+
+# input() imprime un mensaje y recoje la respuesta del usuario.
+input("Presiona cualquier boton para comenzar")
+input("Piensa en un numero, enter para continuar")
+input("Multiplica el numero por 2, enter para continuar")
+input("Suma 10 a ese numero, enter para continuar")
+input("Divide ese numero por 2, enter para continuar")
+input("Restale tu numero inicial, enter para continuar")
+input("El numero que te queda es...")
+print("Tu numero es el 5")
